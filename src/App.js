bf14dfaf00c346c2f1642fea8f5f94e5486562de
Ján Kostor <ko.janci@gmail.com>
2021-12-19 23:22:32 +0100

    primitivny footer

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import React from "react";
 import { Header } from "./sections/header/Header.js";
 import { Showcase } from "./sections/showcase/Showcase.js";
 import { Contact } from "./sections/contact/Contact.js";
+import { Footer } from "./sections/footer/Footer.js";
 import "./css/app.css"
 
-export class App extends React.Component{
+export class App extends React.Component {
     render() {
-        return(
-            <div style={{height: "100%"}}>
-                <Header  />
+        return (
+            <div style={{ height: "100%" }}>
+                <Header />
                 <Showcase />
                 <Contact />
                 {/*
-                 // kontakt
                 // recencie
-                // mapa
                 // pata */}
+                <Footer />
             </div>
         );
     }
---
+++ src/sections/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import '../../css/footer.css';
+
+export class Footer extends React.Component {
+
+    render(){
+        return(
+            <div id="footer">
+                <p> &copy Kostorovci 2021</p>
+            </div>
+        )
+    }
+}
