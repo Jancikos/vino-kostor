071c30c9705b20264354b24fdedbc1062f48173b
Ján Kostor <ko.janci@gmail.com>
2023-10-16 11:32:46 +0200

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    'index.html',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        'index.html',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'primary': "#C4C4C4",
+                'secondary': "#FFFFFF",
+                'black': "#000000",
+                'white': "#FFFFFF"
+            }
+        }
+    },
+    plugins: [],
 }
 
