6a205ff2d0a7047751ff9c0001a5d1564832e1b0
Ján Kostor <46303009+Jancikos@users.noreply.github.com>
2023-10-23 11:18:47 +0200

    v2.0.0 - tailwind (#4)

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        'index.html',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'primary': "#C4C4C4",
+                'secondary': "#FFFFFF",
+                'black': "#000000",
+                'white': "#FFFFFF"
+            }
+        }
+    },
+    plugins: [],
+}
+
