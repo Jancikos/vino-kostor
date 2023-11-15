fde580c96ddca72f93a89d0918b09e300d8180e9
Ján Kostor <ko.janci@gmail.com>
2023-11-15 22:53:19 +0100

    admin: produty -> tabulka

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Model\ProductQuery;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,7 +20,7 @@ class ProductController extends AdminController
         return $this->renderAdminPage(
             'Produkty',
             'products', [
-
+                'products' => ProductQuery::create()->find()
             ]
         );
     }
